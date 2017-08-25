61aa27d30c83b9d9193cce223444cc42f387daea
AQP20171679 <alejandraadriantejada@gmail.com>
2017-08-25 09:09:52 -0500

    Maquetando hasta about

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /*
  * Archivo principal de JS
  */
+var body = document.getElementsByTagName("BODY")[0];
+	body.setAttribute('onscroll','bar()');
+
+var headerHTML = document.getElementById('headerHTML');
+var presentation = document.getElementById('presentation');
+
+function bar() {
+	if (body.scrollTop > 0) {
+		headerHTML.classList.add('header2');
+		presentation.classList.add('presentation2');
+	}else{
+		headerHTML.classList.remove('header2');
+		presentation.classList.remove('presentation2');
+	}
+}
