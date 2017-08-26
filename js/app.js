d84a17556aae9cc75ba3701d57b52182c5dddfb5
AQP20171679 <alejandraadriantejada@gmail.com>
2017-08-25 19:59:30 -0500

    Imagenes generadas

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ var body = document.getElementsByTagName("BODY")[0];
 var headerHTML = document.getElementById('headerHTML');
 var presentation = document.getElementById('presentation');
 
+var imgPortafolio = ['cabin.png','cake.png','circus.png','game.png','safe.png','submarine.png'];
+var portaforlio = document.getElementById('img-portafolio');
+
 function bar() {
 	if (body.scrollTop > 0) {
 		headerHTML.classList.add('header2');
@@ -15,4 +18,17 @@ function bar() {
 		headerHTML.classList.remove('header2');
 		presentation.classList.remove('presentation2');
 	}
+}
+
+var rowPortafolio = document.createElement('div');
+	rowPortafolio.setAttribute('class','row');
+	portaforlio.appendChild(rowPortafolio);
+for (var i = 0; i < imgPortafolio.length; i++) {
+	var divImgPortafolio = document.createElement('div');
+		divImgPortafolio.setAttribute('class','col-4');
+	var img = document.createElement('img');
+		img.src = 'assets/images/portfolio/' + imgPortafolio[i];
+
+	divImgPortafolio.appendChild(img);
+	rowPortafolio.appendChild(divImgPortafolio);
 }
